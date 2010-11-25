f5a3cd41d5b966cc680ae56d545e8dce058db2f7
parweb <parweb@gmail.com>
2010-11-26 00:06:10 +0100

    attribution dune taille par defaut de la fenetre principal

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 #include "Echiquier.h"
 
 int main( int argc, char * argv[] ) {
 	QApplication a( argc, argv );
+
 	MainWindow w;
+
+	w.setEnabled(true);
+	w.resize( 500, 620);
+
 	w.show();
 
 	return a.exec();
